31c0fb4c7b9361830b6fcb417f15e6d3aab08ece
j-o-e-d-o-e <d_joe@gmx.net>
2018-10-03 21:56:57 +0200

    Add project files.

---
+++ MvcSecSql.Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcSecSql.Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
