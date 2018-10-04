5bdfe224dc97e2d45d767204a9cdb2006248033b
j-o-e-d-o-e <d_joe@gmx.net>
2018-10-04 14:39:09 +0200

    entity classes with properties and restrictions added

---
+++ MvcSecSql.Data/Data/Entities/Instructor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcSecSql.Data.Data.Entities
+{
+    public class Instructor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(80), Required]
+        public string Name { get; set; }
+
+        [MaxLength(1024)]
+        public string Description { get; set; }
+
+        [MaxLength(1024)]
+        public string Thumbnail { get; set; }
+
+        public List<Course> Courses { get; set; }
+    }
+}
